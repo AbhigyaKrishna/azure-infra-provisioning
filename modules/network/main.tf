6bfb6d09fdb8969eec9152bbe7b05153d9040250
AbhigyaKrishna <abhigyakrishna21@gmail.com>
2025-10-05 11:45:29 +0530

    feat: network config

---
+++ modules/network/main.tf
@@ -0,0 +1,124 @@
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.project_name}-${var.environment}-vnet"
+  address_space       = var.address_space
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  tags = merge(var.common_tags, {
+    Name        = "${var.project_name}-${var.environment}-vnet"
+    Environment = var.environment
+  })
+}
+
+resource "azurerm_subnet" "private" {
+  name                 = "${var.project_name}-${var.environment}-private-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [var.private_subnet_cidr]
+}
+
+resource "azurerm_subnet" "public" {
+  name                 = "${var.project_name}-${var.environment}-public-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [var.public_subnet_cidr]
+}
+
+resource "azurerm_network_security_group" "vm_nsg" {
+  name                = "${var.project_name}-${var.environment}-public-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = var.public_subnet_cidr
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTPS"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = var.public_subnet_cidr
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowOutbound"
+    priority                   = 100
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = merge(var.common_tags, {
+    Name        = "${var.project_name}-${var.environment}-vm-nsg"
+    Environment = var.environment
+  })
+}
+
+resource "azurerm_subnet_network_security_group_association" "private" {
+  subnet_id                 = azurerm_subnet.private.id
+  network_security_group_id = azurerm_network_security_group.vm_nsg.id
+}
+
+resource "azurerm_public_ip" "nat" {
+  name                = "${var.project_name}-${var.environment}-nat-pip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+
+  tags = merge(var.common_tags, {
+    Name        = "${var.project_name}-${var.environment}-nat-pip"
+    Environment = var.environment
+  })
+}
+
+resource "azurerm_nat_gateway" "main" {
+  name                = "${var.project_name}-${var.environment}-nat-gateway"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku_name            = "Standard"
+
+  tags = merge(var.common_tags, {
+    Name        = "${var.project_name}-${var.environment}-nat-gateway"
+    Environment = var.environment
+  })
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "main" {
+  nat_gateway_id       = azurerm_nat_gateway.main.id
+  public_ip_address_id = azurerm_public_ip.nat.id
+}
+
+resource "azurerm_subnet_nat_gateway_association" "private" {
+  subnet_id      = azurerm_subnet.private.id
+  nat_gateway_id = azurerm_nat_gateway.main.id
+}
---
+++ modules/network/variables.tf
@@ -0,0 +1,40 @@
+variable "project_name" {
+  description = "The name of the project."
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment for the resources (e.g., dev, prod)."
+  type        = string
+}
+
+variable "address_space" {
+  description = "The address space for the virtual network."
+  type        = list(string)
+}
+
+variable "location" {
+  description = "The Azure region where resources will be created."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group in which to create the virtual network."
+  type        = string
+}
+
+variable "common_tags" {
+  description = "A map of common tags to apply to all resources."
+  type        = map(string)
+  default     = {}
+}
+
+variable "private_subnet_cidr" {
+  description = "The CIDR block for the private subnet."
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "The CIDR block for the public subnet."
+  type        = string
+}
